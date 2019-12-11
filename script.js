4c8b00e53b93165efa7f21f59c58152b9705de60
Catherine Adegoke <cmadegoke@gmail.com>
2019-12-11 00:56:20 -0500

    js prompt and confirm actions

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+
+//array of password length 
+var PassLength = []
+
+
+//prompt for password length 
+varPassword = prompt ("what length is the password?")
+if 
+
+//alert for special character
+varSpecialChar = confirm ("Would you like to use a special character")
+
+//alert for lower case
+varLowerCase = confirm ("Would you like to use a lower Case letter?")
+
+//alert for upper case
+varUpperCase = confirm ("Would you like to use a Upper case letter?")
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -20,4 +40,33 @@ function copyToClipboard() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-// BONUS EVENT LISTENER
+// BONUS EVENT LISTENER 
+
+
+
+
+
+
+
+
+
+// create functions for random numbers
+// create functions for alphabets
+
+//the user will be prompted to choose from the following password criteria:
+
+//* Length (must be between 8 and 128 characters)
+
+//* Character type:
+
+  //* Special characters ([see examples](https://www.owasp.org/index.php/Password_special_characters))
+
+ // * Numeric characters
+
+ // * Lowercase characters
+
+ // * Uppercase characters
+
+
+
+
