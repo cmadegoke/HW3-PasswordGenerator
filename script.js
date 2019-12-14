6b03f105d3366b5ca4b4f62e3f616d593ec6cf90
Catherine Adegoke <cmadegoke@gmail.com>
2019-12-14 10:46:18 -0500

    updated JS

--- script.js
+++ script.js
@@ -1,105 +1,93 @@
 
-//ask prompt User question 
+//ask prompt User questions
 
-var Userinput;
-var UserLength = prompt("what length is the password?");
-var UserSpecialChar = confirm("does it contain special character?");
-var UserLowerCase = confirm("does it contain Lower case " );
-var userUpperCase = confirm("does it contain Upper case ");
+var userinput;
+var passwordLength = prompt("what length is the password?");
+//if statement for password length
+//if (passwordLength < 8 && passwordLength > 128) {
+    var containsSpecial = confirm("does it contain special character?");
+    var containsLower = confirm("does it contain Lower case " );
+    var containsUpper = confirm("does it contain Upper case ");
+    var containsNumbers = confirm("does it contain numbers ");
+
+//alert the user about password length
+//} else {
+    alert("Password must be between 8 and 128  characters");
+//}
+
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener('click', writePassword)
 
 
 // // Get arrays
-// var num = [1,2,3,4,5,6,7,8,9,0];
-// var lower_alphabets= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-// var char = ['@' ,'#','$','%','^','&','*','()','{}','[',']','=','<','>','/',','];
-// var upper_alphabets = ['A','B','C','D','E','F','G,'H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var num = [1,2,3,4,5,6,7,8,9,0];
+var lower_alphabets= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var char = ['@','#','$','%','^','&','*','(',')','{','}','[',']','=','<','>','/',','];
+var upper_alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 
 //Functions
 
-function UpperCase(length) {
+function generatePassword() {
 
-    var result           = '';
- 
-    var Uppercharacters  = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
- 
-    var charactersLength = characters.length;
- 
-    for ( var i = 0; i < length; i++ ) {
- 
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
- 
+    if (passwordLength < 8) {
+        return 'Password not long enough';
+    } else if (passwordLength > 158) {
+        return 'Password is too long!'
     }
- 
-    return result;
- 
- }
 
- function lowerCase(length) {
+    if (!containsSpecial && !containsLower && !containsUpper && !containsNumbers) return 'No Characteres Selected'
 
-    var result           = '';
- 
-    var Lowercharacters       = 'abcdefghijklmnopqrstuvwxyz';
- 
-    var charactersLength = characters.length;
- 
-    for ( var i = 0; i < length; i++ ) {
- 
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
- 
+    var count = 0;
+    var password = '';
+    if (containsSpecial) count++
+    if (containsLower) count ++
+    if (containsUpper) count ++
+    if (containsNumbers) count++
+    var charactersPerCategory = Math.floor(passwordLength / count);
+
+    var extraCharacters = passwordLength % count
+
+    //for loop
+    for (var i= 0; i < charactersPerCategory; i++ ){
+        console.log('in the loop')
+        // if contains variable is true, add a random character from its matching array to the password string;
+        if (containsSpecial) {
+            password = password + char[Math.floor(Math.random() * char.length)]
+        }
+        if (containsUpper){
+            password = password + upper_alphabets[Math.floor(Math.random ()* upper_alphabets.length)]
+        }
+        if (containsLower){
+            password = password + lower_alphabets[Math.floor(Math.random ()* lower_alphabets.length)]
+        }
+        if (containsNumbers){
+            password = password + num[Math.floor(Math.random ()* num.length)] 
+        }    
+
     }
- 
-    return result;
- 
- }
- 
-  
- 
- function makeNum(length) {
- 
-    var result           = '';
- 
-    var characters       = '0123456789';
- 
-    var charactersLength = characters.length;
- 
-    for ( var i = 0; i < length; i++ ) {
- 
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
- 
+
+    for (var i = 0; i < extraCharacters; i++) {
+        if (containsLower) {
+            password = password + lower_alphabets[Math.floor(Math.random ()* lower_alphabets.length)]
+        } else if (containsUpper){
+            password = password + upper_alphabets[Math.floor(Math.random ()* upper_alphabets.length)]
+        } else if (containsNumbers){
+            password = password + num[Math.floor(Math.random ()* num.length)] 
+        } else if (containsSpecial) {
+            password = password + char[Math.floor(Math.random() * char.length)]
+        }
     }
- 
-    return result;
- 
- }
- 
-  
- 
- function makeSC(length) {
- 
-    var result           = '';
- 
-    var characters       = '!@#$%^&*():;"?/<>,.';
- 
-    var charactersLength = characters.length;
- 
-    for ( var i = 0; i < length; i++ ) {
- 
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
- 
-    }
- 
-    return result;
- 
- }
+ //   console.log({passwordLength, containsSpecial, containsLower, containsUpper, containsNumbers, count, charactersPerCategory, extraCharacters, password})
+    return password;
+}
  
  function writePassword() {
     var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-    var generateBtn = document.querySelector("#generate");
-
+    // console.log({password})
+    // var passwordText = document.querySelector("#password");
+    document.querySelector("#password").value = password;
     //array of password length 
-    var PassLength = 0
-    passwordText.value = password;
+    // passwordText.value = password;
 
  }
 
